d736f85afa407f59cd63d28d982dd66c4063ff67
K G T <K G T@DESKTOP-JODNND1>
2024-04-29 15:53:33 +0530

    Sample has being modified......

--- src/gitpack/Sample.java
+++ src/gitpack/Sample.java
@@ -4,5 +4,6 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO JAVA PROGRAMMING");
 		System.out.println("modified code 1st time");
+		System.out.println("modified code 2nd time");
 	}
 }
