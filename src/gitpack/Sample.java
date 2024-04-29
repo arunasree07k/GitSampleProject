c2a5ce9f8f981a51e41dd42219b4f0874f81828a
K G T <K G T@DESKTOP-JODNND1>
2024-04-29 15:46:46 +0530

    commiting Sample to BranchZ

--- src/gitpack/Sample.java
+++ src/gitpack/Sample.java
@@ -3,5 +3,6 @@ package gitpack;
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("WELCOME TO JAVA PROGRAMMING");
+		System.out.println("modified code 1st time");
 	}
 }
